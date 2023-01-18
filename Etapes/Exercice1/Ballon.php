8ff52e409187c33d6b9531fb3a725f11cd780b60
JasmiDW <117347250+JasmiDW@users.noreply.github.com>
2023-01-18 14:59:30 +0100

    first commit

---
+++ Etapes/Exercice1/Ballon.php
@@ -0,0 +1,53 @@
+<!-- Créer une classe ballon.
+
+Cette classe contient :
+
+Attributs : couleur et nom
+Méthodes : constructeur, getters et setters, getInfos et changeCouleur
+Créer un scénario permettant de :
+
+créér un ballon rouge qui s’appelle Bob
+créer un ballon bleu qui s’appelle Toby
+Toby change Bob en vert via héritage
+Afficher les états à chaque changement. -->
+
+<?php
+
+    class Ballon {
+
+        protected $_nom;
+        protected $_couleur;
+
+        public function __construct($nom,$couleur){
+            $this-> setNom($nom);
+            $this->setCouleur($couleur);
+        }
+
+        public function getNom(){
+            return $this->_nom;
+        }
+
+        public function setNom($new_nom){
+            $this->_nom=$new_nom;
+
+        }
+        public function getCouleur(){
+            return $this->_couleur;
+        }
+
+        public function setCouleur($new_couleur){
+            $this->_couleur=$new_couleur;
+
+        }
+        public function getInfos(){
+            return "Le ballon " .$this-> getCouleur(). " s'appelle " .$this->getNom(). ".<br>";
+        }
+
+        public function changeCouleur($cible,$couleur){
+            $cible->setCouleur($couleur);
+        }
+
+
+    }
+
+?>
